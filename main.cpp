0b2393dcd87619df107dd4738ca2901d7f6a3f28
ArturRejment <arturrejment1@gmail.com>
2022-03-24 22:42:37 +0100

    feat: move the ball and change background collor

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <ncurses.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "Ball/ball.cpp"
 
 using namespace std;
-
 
 int main(int argc, char** argv) {
 	Ball ball = Ball("O");
@@ -17,18 +17,26 @@ int main(int argc, char** argv) {
 
 	WINDOW *win = newwin(yMax/2, xMax/2, yMax/4, xMax/4);
 	box(win, 0, 0);
+	init_pair(1, COLOR_RED, COLOR_YELLOW);
+	start_color();
+	attron(COLOR_PAIR(1));
+	// wbkgdset(win, COLOR_GREEN);
+	// refresh();
 
-	for (int i=2; i<12; i++){
+	for (int i=2; i<20; i++){
 		mvwprintw(win, ball.getXPosition(), ball.getYPosition(), ball.getName());
 		ball.setXPosition(i);
 		ball.setYPosition(i);
 		wrefresh(win);
 		fflush(stdout);
-		napms(200);
+		napms(500);
+		werase(win);
+		box(win, 0, 0);
 	}
 
 	wgetch(win);
 
+	attroff(COLOR_PAIR(1));
 	endwin();
 	return 0;
 }
